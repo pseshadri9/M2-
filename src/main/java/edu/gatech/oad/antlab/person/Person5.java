b2b964d39e9f3a4cb5f48b011172340a77ab616f
Nicholas714 <grananicholas@gmail.com>
2019-01-23 15:29:04 -0500

    added string shifter to Person5.java

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -30,8 +30,9 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+		String prefix = input.substring(0, 2);
+		input = input.substring(2) + prefix;
+	  return input;
 	}
 	
 	/**
