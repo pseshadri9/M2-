c7f12475e655a083dba6997efcd014c1d76f5a58
Nicholas714 <grananicholas@gmail.com>
2019-01-23 15:35:44 -0500

    added Nicholas Grana to AntLabMain

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Nicholas Grana");
+                  toPrint += p5.toString("ngrana3");
 		  
         System.out.println(toPrint);
         
