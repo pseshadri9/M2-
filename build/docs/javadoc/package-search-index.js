de89327d652cf47d0192bbb08ca94ab4f1c8b464
Nicholas714 <grananicholas@gmail.com>
2019-01-25 15:58:22 -0500

    fixed javadocs not compiling

---
+++ build/docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"edu.gatech.oad.antlab.person"},{"l":"edu.gatech.oad.antlab.pkg1"},{"l":"edu.gatech.oad.antlab.pkg2"}]
--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -84,6 +84,7 @@ import edu.gatech.oad.antlab.pkg2.*;
     
     /**
      * entry point for the program
+     * @param args values passed through command line
      */
      public static void main(String[] args) {
         
